c96c3bee3ef39992b97224b63b62dfe651b4b6af
SereneParadox <julianbrown.1002@gmail.com>
2025-03-19 20:54:23 -0500

    function display

--- chess-with-julian/board.gd
+++ chess-with-julian/board.gd
@@ -1,6 +1,9 @@
  extends Sprite2D
 
 const BOARD_SIZE = 8
+const Cell_width = 18
+
+"res://Scenes/texture_holder.tscn"
 
 const BLACK_BISHOP = preload("res://Assets/black_bishop.png")
 const BLACK_KING = preload("res://Assets/black_king.png")
@@ -43,9 +46,8 @@ const PIECE_MOVE = preload("res://Assets/Piece_move.png")
 var board : Array
 var white : bool
 var state : bool
-var moves + []
+var moves = []
 var selected_piece = Vector2
-
 
 func _ready():
 	board.append([4,2,3,5,6,3,2,4])
@@ -58,4 +60,34 @@ func _ready():
 	board.append([-4,-2,-3,-5,-6,-3,-2, -4])
 	
 	display_board()
+	
+func display_board():
+	for child in pieces.get_children():
+		child.queue_free()
+	
+	for i in BOARD_SIZE:
+		for j in BOARD_SIZE:
+			var holder = TEXTURE_HOLDER.instantiate()
+			pieces.add_child(holder)
+			holder.global_position = Vector2(j * CELL_WIDTH + (CELL_WIDTH / 2), -i * CELL_WIDTH - (CELL_WIDTH / 2))
+			
+			match board[i][j]:
+				-6: holder.texture = BLACK_KING
+				-5: holder.texture = BLACK_QUEEN
+				-4: holder.texture = BLACK_ROOK
+				-3: holder.texture = BLACK_BISHOP
+				-2: holder.texture = BLACK_KNIGHT
+				-1: holder.texture = BLACK_PAWN
+				0: holder.texture = null
+				6: holder.texture = WHITE_KING
+				5: holder.texture = WHITE_QUEEN
+				4: holder.texture = WHITE_ROOK
+				3: holder.texture = WHITE_BISHOP
+				2: holder.texture = WHITE_KNIGHT
+				1: holder.texture = WHITE_PAWN
+				
+	if white: turn.texture = TURN_WHITE
+	else: turn.texture = TURN_BLACK
+
+func show_options():
 	
